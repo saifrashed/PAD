33d36d4b8af53a2739dee7b3b89009a266af4b01
Saif <saifeddinerashed@icloud.com>
2021-02-10 18:29:30 +0100

    code refactored

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-$(function() {
-	'use strict';
+$(function () {
+    'use strict';
 
-  $('.js-menu-toggle').click(function(e) {
+    $('.js-menu-toggle').click(function (e) {
 
-  	var $this = $(this);
+        var $this = $(this);
 
-  	if ( $('body').hasClass('show-sidebar') ) {
-  		$('body').removeClass('show-sidebar');
-  		$this.removeClass('active');
-  	} else {
-  		$('body').addClass('show-sidebar');
-  		$this.addClass('active');
-  	}
+        if ($('body').hasClass('show-sidebar')) {
+            $('body').removeClass('show-sidebar');
+            $this.removeClass('active');
+        } else {
+            $('body').addClass('show-sidebar');
+            $this.addClass('active');
+        }
 
-  	e.preventDefault();
+        e.preventDefault();
 
-  });
+    });
 
-  // click outisde offcanvas
-	$(document).mouseup(function(e) {
-    var container = $(".sidebar");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ( $('body').hasClass('show-sidebar') ) {
-				$('body').removeClass('show-sidebar');
-				$('body').find('.js-menu-toggle').removeClass('active');
-			}
-    }
-	});
+    // click outisde offcanvas
+    $(document).mouseup(function (e) {
+        var container = $(".sidebar");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('body').hasClass('show-sidebar')) {
+                $('body').removeClass('show-sidebar');
+                $('body').find('.js-menu-toggle').removeClass('active');
+            }
+        }
+    });
 });
 

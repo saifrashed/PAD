0f096b04420845aedc0d21fda0de478ea014d56c
Marvin Hesselmans <mmhesselmans@gmail.com>
2021-06-14 01:27:58 +0200

    Test Rating Marvin Cypress | Versie 2

--- server/cypress/integration/pad/testRating.js
+++ server/cypress/integration/pad/testRating.js
@@ -3,26 +3,15 @@ describe("testRating", function () {
     //Run before each test in this context
     beforeEach(() => {
         //Go to the specified URL
-        cy.visit("http://localhost:63342/team-7/PAD/src/index.html?_ijt=gldru2tvimshmfsnvvjsdefhmf#landing");
+        cy.visit("http://localhost:63342/team-7/PAD/src/index.html?_ijt=ephb62vghnljpce73rukgcsjfs#landing");
+
     });
-
-    //Test: Test login form
-    it("Test login form", function () {
-        cy.get("input[name=login-username]").should("exist");
-
-        cy.get("input[name=login-password]").should("exist");
-
-        cy.get(".login-form button").should("exist");
-    });
-
 
     //Test: Test Rating
-    it("Test Rating", function () {
+    it("Test Successful rating", function () {
         cy.server();
 
         cy.get('.modal').invoke('show'); // Invoke the jQuery 'show' function
-
-        cy.route("POST", "/user/login", {"username": "DunstopMe"}).as("login");
 
         cy.get("input[name=login-username]").type("DunstopMe");
 
@@ -30,26 +19,32 @@ describe("testRating", function () {
 
         cy.get(".login-form button").click();
 
-        cy.wait("@login");
+        cy.get('.burger').click();
+        cy.get('.spellenLink').click();
+        cy.get('.brick a[data-id="1"]').click();
 
-        cy.get("@login").should((xhr) => {
-            expect(xhr.request.body.username).equals("DunstopMe");
+        cy.wait(2500);
 
-            expect(xhr.request.body.password).equals("Kaily00!");
-        });
+        cy.get('.stars i[data-value="4"]').click();
 
-        cy.url().should("contain", "#landing");
+        //After a rating, an element containing our success-message should be shown.
+        cy.get(".notie-alert").should("exist")
+    });
 
+    //Test: Test Rating
+    it("Test failed rating", function () {
+        cy.server();
 
+        cy.get('.burger').click();
+        cy.get('.spellenLink').click();
+        cy.get('.brick a[data-id="1"]').click();
 
+        cy.wait(2500);
 
-        cy.get('.burger').click()
-        cy.get('.spellenLink').click()
-        cy.get('.brick a[data-id="1"]').click()
-        cy.get('.stars i[data-value="4"]').click()
+        cy.get('.stars i[data-value="4"]').click();
 
+        //After a rating, an element containing our success-message should be shown.
+        cy.get(".modal").should("exist")
     })
-
-
 
 });
